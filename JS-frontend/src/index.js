e69d94df0948d161f80a7a2fe0b0cc51725da1bd
Andrew Santos <asantos@alumni.nd.edu>
2019-09-09 17:21:12 -0400

    Added fetch for grid

--- JS-frontend/src/index.js
+++ JS-frontend/src/index.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function(){
     let score = 10;
     // const goButton = document.querySelector('.go')
     let timerEvent = null
+    let mapInfo = null
 
     //WHERE YOU START THE TIME
     // goButton.addEventListener('click', () =>{
@@ -24,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function(){
      }
 
     createGrid();
-    renderBot(currentPosition);
+    // setTimeout(renderBot(currentPosition), 1000);
     
     document.addEventListener("keydown", logKey);
       function logKey(e) {
@@ -48,103 +49,108 @@ document.addEventListener("DOMContentLoaded", function(){
   
     // document.querySelector("#move-button").addEventListener("click", moveRobot)
     
-    function moveRobot(){
-      setInterval(function(){
-        let direction = document.querySelectorAll("li")[0];
-        move(direction.innerText);
-        direction.remove();
-      }, 500)  
-    } 
+    // function moveRobot(){
+    //   setInterval(function(){
+    //     let direction = document.querySelectorAll("li")[0];
+    //     move(direction.innerText);
+    //     direction.remove();
+    //   }, 500)  
+    // } 
       
     ///////////////////////
 
 
-function createGrid(){
-  const board = document.querySelector("#board")
-  let mapInfo = [['O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'], 
-                 ['O', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O'],
-                 ['O', 'I', 'O', 'I', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-                 ['O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O'],
-                 ['O', 'I', 'O', 'O', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'I', 'O'],
-                 ['O', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'O'],
-                 ['O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'O', 'O', 'I', 'O'],
-                 ['O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'O'],
-                 ['O', 'O', 'O', 'I', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O'],
-                 ['O', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O'],
-                 ['O', 'O', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O'],
-                 ['O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'O'],
-                 ['O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
-                 ['O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O'],
-                 ['O', 'O', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O'],
-                 ['O', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'O'],
-                 ['O', 'I', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'I', 'O'],
-                 ['O', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'W'],
-                 ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']]
-  for (let y=0; y < 19; y++){
-    for (let x=0; x < 19; x++){
-      board.insertAdjacentHTML('beforeend', `
-        <div class="tile" data-x=${x} data-y=${y} id=${mapInfo[y][x]}></div>
-      `)
+    function createGrid(){
+    const board = document.querySelector("#board")
+    fetch('http://localhost:3000/mazes/1')
+    .then(resp => resp.json())
+    .then(maze => {
+        mapInfo = JSON.parse(maze.grid)
+        console.dir(typeof mapInfo)
+    //   let mapInfo = [['O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'], 
+    //                  ['O', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O'],
+    //                  ['O', 'I', 'O', 'I', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+    //                  ['O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O'],
+    //                  ['O', 'I', 'O', 'O', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'O', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'O'],
+    //                  ['O', 'O', 'O', 'I', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O'],
+    //                  ['O', 'O', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+    //                  ['O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O'],
+    //                  ['O', 'O', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'O', 'O', 'O', 'O', 'I', 'O', 'I', 'O', 'I', 'O'],
+    //                  ['O', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'O', 'I', 'O', 'I', 'W'],
+    //                  ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']]
+        for (let y=0; y < 19; y++){
+            for (let x=0; x < 19; x++){
+            board.insertAdjacentHTML('beforeend', `
+                <div class="tile" data-x=${x} data-y=${y} id=${mapInfo[y][x]}></div>
+            `)
+            }
+        }
+        renderBot(currentPosition)
+        })
     }
-  }
-}
+    
+    function renderBot(targetPosition){
+        const tiles = document.querySelectorAll(".tile")
 
-function renderBot(targetPosition){
-  const tiles = document.querySelectorAll(".tile")
+        const newTile = tiles.find(function(tile){
+            return parseInt(tile.dataset.x) === targetPosition.x && parseInt(tile.dataset.y) === targetPosition.y
+        })
 
-  const newTile = tiles.find(function(tile){
-    return parseInt(tile.dataset.x) === targetPosition.x && parseInt(tile.dataset.y) === targetPosition.y
-  })
+        // console.dir(newTile)
 
-  // console.dir(newTile)
+        if (!newTile || newTile.id === "O"){
+            return false
+        } 
+        else {
+            if (prevTile){
+            prevTile.id = "I"
+            } 
+            if (newTile.id === "W"){
+            setTimeout(function(){alert((`YOU'RE SCORE IS: ${score}`), 10000)})
+            clearInterval(timerEvent)
+            }
 
-  if (!newTile || newTile.id === "O"){
-    return false
-  } 
-  else {
-    if (prevTile){
-      prevTile.id = "I"
-    } 
-    if (newTile.id === "W"){
-      setTimeout(function(){alert((`YOU'RE SCORE IS: ${score}`), 10000)})
-      clearInterval(timerEvent)
+        newTile.id = "robot"
+        prevTile = newTile
+
+        return true
+        }
+
     }
 
-  newTile.id = "robot"
-  prevTile = newTile
+    function move(direction){
+    let x = currentPosition.x;
+    let y = currentPosition.y;
+    switch(direction){
+        case "left":
+        x--
+        break;
+        case "right":
+        x++
+        break;
+        case "up":
+        y--
+        break;
+        case "down":
+        y++
+        break;
+    }
 
-  return true
-  }
+    const moved = renderBot({ x, y })
+    if (moved){
+        currentPosition = { x, y }
+    }
+    }
 
-}
-
-function move(direction){
-  let x = currentPosition.x;
-  let y = currentPosition.y;
-  switch(direction){
-    case "left":
-      x--
-      break;
-    case "right":
-      x++
-      break;
-    case "up":
-      y--
-      break;
-    case "down":
-      y++
-      break;
-  }
-
-  const moved = renderBot({ x, y })
-  if (moved){
-    currentPosition = { x, y }
-  }
-}
-
-
-
-  });
+});
   
   
 /////////////
